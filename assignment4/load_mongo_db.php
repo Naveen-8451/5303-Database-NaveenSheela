0cf7584d2f480bdbb169ee09e4108c991f7bb0b4
Naveen0217 <shella.naveen56@gmail.com>
2015-09-30 10:56:51 -0500

    Update load_mongo_db.php

--- assignment4/load_mongo_db.php
+++ assignment4/load_mongo_db.php
@@ -2,18 +2,18 @@
 <?php
 /*
 Name: Naveen kumar sheela
-assignment: 4
-Course: Advance DataBase System
+assignment4
+Course: 5303 Advance DataBase System
 */
 
-    $m = new MongoClient();												// connect
-    $db = $m->nsheela;													// select a database
+    $m = new MongoClient();						// connection string for mongoDB
+    $db = $m->nsheela;							// connection to local datbase database name="nsheela"
     $db = $m->nsheela;
-	$collection = $db->random_people;									// select a collection (analogous to a relational database's table)
-	$json = file_get_contents("http://api.randomuser.me/?results=1000");// fetching the data and loading into the array
-    $json_array = json_decode($json);									// looping to insert the document into the database.    
-	for($i=0;$i<sizeof($json_array->results);$i++)
+    $collection = $db->random_people;					// select a collection from mongoDB that to be inserted 
+    $json = file_get_contents("http://api.randomuser.me/?results=1000");// fetching the data and loading into the array
+    $json_array = json_decode($json);					// Jason array to collect random_people information    
+	for($i=0;$i<sizeof($json_array->results);$i++)			//loop repeating for 1000+ users	
     {
-        $collection->insert($json_array->results[$i]);
+        $collection->insert($json_array->results[$i]);			//inserting into mongoDb
     }   
 ?>
