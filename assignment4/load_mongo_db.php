9e4c274a210258ff8f0926cd93cb7af980970e9d
Naveen0217 <shella.naveen56@gmail.com>
2015-09-30 10:57:24 -0500

    Update load_mongo_db.php

--- assignment4/load_mongo_db.php
+++ assignment4/load_mongo_db.php
@@ -8,7 +8,6 @@ Course: 5303 Advance DataBase System
 
     $m = new MongoClient();						// connection string for mongoDB
     $db = $m->nsheela;							// connection to local datbase database name="nsheela"
-    $db = $m->nsheela;
     $collection = $db->random_people;					// select a collection from mongoDB that to be inserted 
     $json = file_get_contents("http://api.randomuser.me/?results=1000");// fetching the data and loading into the array
     $json_array = json_decode($json);					// Jason array to collect random_people information    
