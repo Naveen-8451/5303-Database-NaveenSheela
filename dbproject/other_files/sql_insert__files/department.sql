7de424e3dcba3f81ea25d5b2e9e88c51993227cb
Naveen0217 <NaveenKumarShella@Naveens-MacBook-Pro.local>
2015-12-04 03:24:54 -0600

    Updated

---
+++ dbproject/other_files/sql_insert__files/department.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: 
+-- Date: 2015-12-04 03:13
+*/
+INSERT INTO `department` (`dept_id`,`dept_name`) VALUES (dept_id,'dept_name');
+INSERT INTO `department` (`dept_id`,`dept_name`) VALUES (1,'Information Technology');
+INSERT INTO `department` (`dept_id`,`dept_name`) VALUES (2,'Human Resources');
+INSERT INTO `department` (`dept_id`,`dept_name`) VALUES (3,'Finance');
+INSERT INTO `department` (`dept_id`,`dept_name`) VALUES (4,'General Admin');
---
+++ dbproject/other_files/sql_insert__files/user_feedback.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: 
+-- Date: 2015-12-04 03:13
+*/
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (6,19,'internet site','http://google.com','Driver nor friendly','2015-12-02',2);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (21,79,'employer','NULL','Driver nor friendly','2015-11-23',1);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (3,9,'friend','NULL','I\'ll give it 3/5 rating','2015-12-02',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (9,31,'employer','NULL','I\'ll give it 3/5 rating','2015-11-13',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (14,51,'internet site','http://google.com','I\'ll give it 3/5 rating','2015-11-23',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (18,68,'internet site','http://google.com','I\'ll give it 3/5 rating','2015-11-11',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (24,91,'employer','NULL','I\'ll give it 3/5 rating','2015-11-23',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (4,11,'employer','NULL','Love the concept !','2015-11-11',4);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (10,36,'internet site','http://google.com','Love the concept !','2015-12-02',4);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (19,71,'friend','NULL','Love the concept !','2015-11-23',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (25,96,'internet site','http://google.com','Love the concept !','2015-12-02',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (2,6,'internet site','http://google.com','Need some improvement on support','2015-11-18',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (8,28,'employer','NULL','Need some improvement on support','2015-11-13',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (13,48,'employer','NULL','Need some improvement on support','2015-11-23',2);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (17,65,'employer','NULL','Need some improvement on support','2015-11-02',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (23,88,'friend','NULL','Need some improvement on support','2015-11-23',3);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (15,56,'friend','NULL','Nice','2015-11-23',4);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (7,25,'friend','NULL','Smelly car','2015-11-11',2);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (22,85,'internet site','http://google.com','Smelly car','2015-12-02',2);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (1,1,'employer','NULL','Very good !','2015-11-11',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (12,45,'employer','NULL','Very good !','2015-11-11',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (16,59,'employer','NULL','Very good !','2015-12-02',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (5,16,'employer','NULL','Will do it again','2015-11-13',4);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (11,39,'friend','NULL','Will do it again','2015-11-23',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (20,76,'employer','NULL','Will do it again','2015-11-23',5);
+INSERT INTO `user_feedback` (`feedback_id`,`user_id`,`source`,`website_name`,`notes`,`date`,`rating`) VALUES (26,99,'friend','NULL','Will do it again','2015-11-23',4);
